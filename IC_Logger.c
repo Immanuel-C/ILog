8c485cc4e637184db0a968c95d832f90fdaadda2
Immanuel Charles <73500956+immanuelcharlesiscool@users.noreply.github.com>
2022-02-21 09:06:08 -0800

    Init Commit

---
+++ IC_Logger.c
@@ -0,0 +1,64 @@
+#include "Logger.hpp"
+
+void _windowsLog(const char* msg, ConsoleColour colour) {
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    uint32_t colourBits = 0;
+
+    switch (colour) {
+        case ConsoleColour::WHITE: {
+            colourBits = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
+            break;
+        }
+        case ConsoleColour::GREEN: {
+            colourBits = FOREGROUND_GREEN;
+            break;
+        }
+        case ConsoleColour::YELLOW: {
+            colourBits = FOREGROUND_GREEN | FOREGROUND_RED;
+            break;
+        }
+        case ConsoleColour::RED: {
+            colourBits = FOREGROUND_RED;
+            break;
+        }
+        case ConsoleColour::FATAL_RED: {
+            colourBits = COMMON_LVB_UNDERSCORE | BACKGROUND_RED | BACKGROUND_INTENSITY;
+            break;
+        }
+    };
+
+    SetConsoleTextAttribute(consoleHandle, colourBits);
+
+    WriteConsoleA(consoleHandle, msg, strlen(msg), nullptr, nullptr);
+
+    SetConsoleTextAttribute(consoleHandle, 
+    FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+}
+
+void _log(const char* prefix, const char* msg, ConsoleColour colour, ...) {
+    char* fmtBuffer = new char[ strlen(prefix) + strlen(msg) ];
+    // Copy formated string into the fmtBuffer
+    sprintf(fmtBuffer, "%s: %s", prefix, msg);
+
+    va_list args;
+
+    va_start(args, colour);
+
+    // returns the length of the formated string
+    // I'm not sure why you have to add one to this but I
+    // think _vscprintf doesn't count \0 as a character
+    // but vsprintf_s needs that \0 
+    size_t msgLen = _vscprintf(fmtBuffer, args) + 1;
+    
+    char* msgBuffer = new char[msgLen];
+    
+    // This is different from sprintf becuase it takes in a va_list instead of variable args
+    vsprintf_s(msgBuffer, msgLen, fmtBuffer, args);
+
+    va_end(args);
+
+    _windowsLog(msgBuffer, colour);
+
+    printf("\n");
+}
---
+++ IC_Logger.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #error "This logger only supports windows..." 
+#endif // _WIN32
+
+
+enum struct ConsoleColour {
+    WHITE,
+    GREEN,
+    YELLOW,
+    RED,
+    FATAL_RED
+};
+
+#ifndef NDEBUG
+
+#define IC_DEBUG_LOG_TRACE(msg, ...) _log("Debug Trace", msg, ConsoleColour::GREEN, __VA_ARGS__)
+#define IC_DEBUG_LOG_INFO(msg, ...)  _log("Debug Info", msg, ConsoleColour::WHITE, __VA_ARGS__)
+#define IC_DEBUG_LOG_WARNING(msg, ...)  _log("Debug Warning", msg, ConsoleColour::YELLOW, __VA_ARGS__)
+#define IC_DEBUG_LOG_ERROR(msg, ...)  _log("Debug Error", msg, ConsoleColour::RED, __VA_ARGS__)
+#define IC_DEBUG_LOG_FATAL_ERROR(msg, ...)  _log("Debug Fatal Error", msg, ConsoleColour::FATAL_RED, __VA_ARGS__)
+
+#else
+
+#define IC_DEBUG_LOG_TRACE(msg, ...)
+#define IC_DEBUG_LOG_INFO(msg, ...)
+#define IC_DEBUG_LOG_WARNING(msg, ...)
+#define IC_DEBUG_LOG_ERROR(msg, ...)
+#define IC_DEBUG_LOG_FATAL_ERROR(msg, ...)
+
+#endif
+
+#define IC_LOG_TRACE(msg, ...) _log("Trace", msg, ConsoleColour::GREEN, __VA_ARGS__)
+#define IC_LOG_INFO(msg, ...)  _log("Info", msg, ConsoleColour::WHITE, __VA_ARGS__)
+#define IC_LOG_WARNING(msg, ...)  _log("Warning", msg, ConsoleColour::YELLOW, __VA_ARGS__)
+#define IC_LOG_ERROR(msg, ...)  _log("Error", msg, ConsoleColour::RED, __VA_ARGS__)
+#define IC_LOG_FATAL_ERROR(msg, ...)  _log("Fatal Error", msg, ConsoleColour::FATAL_RED, __VA_ARGS__)
+
+
+// Do not use
+extern void _windowsLog(const char* msg, ConsoleColour colour);
+
+// Do not use
+extern void _log(const char* prefix, const char* msg, ConsoleColour colour, ...);
