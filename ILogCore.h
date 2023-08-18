83c13f16526094817c1a4b6c357dc518eca962d3
Immanuel Charles <73501113+Immanuel-C@users.noreply.github.com>
2023-08-18 17:22:57 -0400

    Fixed not being able to make a static library

--- ILogCore.h
+++ ILogCore.h
@@ -2,13 +2,15 @@
 #define ILOG_CORE_H
 
 #if defined(_WIN32)
-
+#if defined(ILOG_DLL)
 #if defined(ILOG_BUILD_DLL)
 #define ILOG_API __declspec(dllexport)
 #else 
 #define ILOG_API __declspec(dllimport)
 #endif
-
+#else
+#define ILOG_API
+#endif
 #endif
 
 #endif
